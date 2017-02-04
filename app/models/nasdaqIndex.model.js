af45db54dbb6c53fdeaac27351834f635daaffae
Perajit Pasupat <perajit.pasupat@thomsonreuters.com>
2017-02-04 23:52:50 +0700

    Define schema

---
+++ app/models/nasdaqIndex.model.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = require('bluebird');
+
+var NasdaqIndexSchema = new Schema({
+  indexName: { type: String },
+  dateTime: { type: Date, default: Date.now },
+  value: { type: Number },
+  netChange: { type: Number },
+  pctChange: { type: Number }
+});
+
+module.exports = mongoose.model('NasdaqIndex', NasdaqIndexSchema);
