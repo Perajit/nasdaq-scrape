1f55c60f01c7f1af80f7dfe68942b6210b7ee11f
Perajit Pasupat <perajit.pasupat@thomsonreuters.com>
2017-02-06 01:51:26 +0700

    Add more test and refactor

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -10,11 +10,15 @@ var nasaqIndexService = require('../../app/services/nasdaqIndex');
 var test = function(config) {
   var app = apiProcess(config);
   var port = config.API_DEFAULT_PORT;
+  var start = '2017-02-05T10:19:12.703Z';
+  var end = '2017-02-05T10:20:11.238Z';
+  var getDataByTimeRange;
 
   describe('GET nasdaq/:index', function() {
     before(function(done) {
+      getDataByTimeRange = sinon.spy(nasaqIndexService, 'getDataByTimeRange');
+
       this.server = app.listen(port, function() {
-        console.log('***** Started server on port ' + port);
         loadFixture(NasaqIndexModel, nasdaqIndexes)
           .then(function() {
             done();
@@ -26,6 +30,8 @@ var test = function(config) {
     });
 
     after(function(done) {
+      getDataByTimeRange.restore();
+
       this.server.close(function() {
         NasaqIndexModel.remove({})
           .then(function() {
@@ -37,18 +43,59 @@ var test = function(config) {
       });
     });
 
-    it('return all data', function(done) {
+    it('return all data of all index', function(done) {
       var url = 'http://localhost:' + port + '/api/nasdaq/all';
 
       request(url,
         function(err, res, body) {
           var actualData = JSON.parse(body).data;
 
-          nasaqIndexService.getDataByTimeRange(null, null, null)
-            .then(function(data) {
-              expect(JSON.stringify(actualData)).to.equal(JSON.stringify(data));
-              done(err);
-            });
+          expect(res.statusCode).to.equal(200);
+          expect(nasaqIndexService.getDataByTimeRange.calledWith(null, null, null)).to.be.true;
+
+          done();
+        });
+    });
+
+    it('return data of all index from starting time to ending time', function(done) {
+      var url = 'http://localhost:' + port + '/api/nasdaq/all?start=' + start + '&end=' + end;
+
+      request(url,
+        function(err, res, body) {
+          var actualData = JSON.parse(body).data;
+
+          expect(res.statusCode).to.equal(200);
+          expect(getDataByTimeRange.calledWith(null, new Date(start), new Date(end))).to.be.true;
+
+          done();
+        });
+    });
+
+    it('return all data of specified index', function(done) {
+      var url = 'http://localhost:' + port + '/api/nasdaq/NASDAQ';
+
+      request(url,
+        function(err, res, body) {
+          var actualData = JSON.parse(body).data;
+
+          expect(res.statusCode).to.equal(200);
+          expect(getDataByTimeRange.calledWith('NASDAQ', null, null)).to.be.true;
+
+          done();
+        });
+    });
+
+    it('return data of specified index from starting time to ending time', function(done) {
+      var url = 'http://localhost:' + port + '/api/nasdaq/NASDAQ?start=' + start + '&end=' + end;
+
+      request(url,
+        function(err, res, body) {
+          var actualData = JSON.parse(body).data;
+
+          expect(res.statusCode).to.equal(200);
+          expect(getDataByTimeRange.calledWith('NASDAQ', new Date(start), new Date(end))).to.be.true;
+
+          done();
         });
     });
   });
--- test/services/nasdaqIndex.test.js
+++ test/services/nasdaqIndex.test.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
 var loadFixture = require('mongoose-fixture-loader');
 var NasaqIndexModel = require('../../app/models/nasdaqIndex');
-var nasaqIndexService = require('../../app/services/nasdaqIndex');
 var nasdaqIndexes = require('../fixtures/nasdaqIndexes');
+var nasaqIndexService = require('../../app/services/nasdaqIndex');
 
 describe('NasdaqInex service', function() {
   var start = '2017-02-05T10:19:12.703Z';
@@ -29,7 +29,7 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return all data of all index', function(done) {
+  it('should return all data of all index', function(done) {
     nasaqIndexService.getDataByTimeRange(null, null, null)
       .then(function(data) {
         var actualData = data;
@@ -42,12 +42,13 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return data of all index from starting time', function(done) {
+  it('should return data of all index from starting time', function(done) {
     nasaqIndexService.getDataByTimeRange(null, start, null)
       .then(function(data) {
         var actualData = data;
         var expectedData = nasdaqIndexes.filter(function(item) {
-          return new Date(item.dateTime) >= new Date(start);
+          var itemDateTime = new Date(item.dateTime);
+          return itemDateTime >= new Date(start);
         });
 
         expect(JSON.stringify(actualData))
@@ -57,12 +58,13 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return data of all index until ending time', function(done) {
+  it('should return data of all index until ending time', function(done) {
     nasaqIndexService.getDataByTimeRange(null, null, end)
       .then(function(data) {
         var actualData = data;
         var expectedData = nasdaqIndexes.filter(function(item) {
-          return new Date(item.dateTime) <= new Date(end);
+          var itemDateTime = new Date(item.dateTime);
+          return itemDateTime <= new Date(end);
         });
 
         expect(JSON.stringify(actualData))
@@ -72,7 +74,23 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return all data of specified index', function(done) {
+  it('should return data of all index from starting time until ending time', function(done) {
+    nasaqIndexService.getDataByTimeRange(null, start, end)
+      .then(function(data) {
+        var actualData = data;
+        var expectedData = nasdaqIndexes.filter(function(item) {
+          var itemDateTime = new Date(item.dateTime);
+          return itemDateTime >= new Date(start) && itemDateTime <= new Date(end);
+        });
+
+        expect(JSON.stringify(actualData))
+          .to.equal(JSON.stringify(expectedData));
+
+        done();
+      });
+  });
+
+  it('should return all data of specified index', function(done) {
     nasaqIndexService.getDataByTimeRange('NASDAQ', null, null)
       .then(function(data) {
         var actualData = data;
@@ -87,12 +105,13 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return data of specified index from starting time', function(done) {
+  it('should return data of specified index from starting time', function(done) {
     nasaqIndexService.getDataByTimeRange('NASDAQ', start, null)
       .then(function(data) {
         var actualData = data;
         var expectedData = nasdaqIndexes.filter(function(item) {
-          return item.indexName === 'NASDAQ' && new Date(item.dateTime) >= new Date(start);
+          var itemDateTime = new Date(item.dateTime);
+          return item.indexName === 'NASDAQ' && itemDateTime >= new Date(start);
         });
 
         expect(JSON.stringify(actualData))
@@ -102,12 +121,29 @@ describe('NasdaqInex service', function() {
       });
   });
 
-  it('return data of specified index until ending time', function(done) {
+  it('should return data of specified index until ending time', function(done) {
     nasaqIndexService.getDataByTimeRange('NASDAQ', null, end)
       .then(function(data) {
         var actualData = data;
         var expectedData = nasdaqIndexes.filter(function(item) {
-          return item.indexName === 'NASDAQ' && new Date(item.dateTime) <= new Date(end);
+          var itemDateTime = new Date(item.dateTime);
+          return item.indexName === 'NASDAQ' && itemDateTime <= new Date(end);
+        });
+
+        expect(JSON.stringify(actualData))
+          .to.equal(JSON.stringify(expectedData));
+
+        done();
+      });
+  });
+
+  it('should return data of specified index from starting time until ending time', function(done) {
+    nasaqIndexService.getDataByTimeRange('NASDAQ', start, end)
+      .then(function(data) {
+        var actualData = data;
+        var expectedData = nasdaqIndexes.filter(function(item) {
+          var itemDateTime = new Date(item.dateTime);
+          return item.indexName === 'NASDAQ' && itemDateTime >= new Date(start) && itemDateTime <= new Date(end);
         });
 
         expect(JSON.stringify(actualData))
